87488f6b634fc1b239db66863ec2198287dd0a42
wenguangshan <wensjtuer@gmail.com>
2016-07-23 23:13:01 +0800

    fix some typo

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 
 #include "lexer.h"
 #include "parser.h"
-using namespace std;
-
-string source_code;
 
 
 vector<Tokener> t_vec;
+string source_code;
 
 int main()
 {
 	ifstream f("hello.txt");
 
-	int ;
 
 	init(f,source_code);
 
 	string_to_token(source_code,t_vec);
 	
-	 print();
+	 print();//¥Ú”°token
 
 	cout << endl;
 	system("pause");
